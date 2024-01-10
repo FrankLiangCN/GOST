5af245863d09372528959329f020afddf04d2236
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-01-11 00:05:50 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 systemctl stop gost
+systemctl disable gost
+rm /etc/systemd/system/gost.service -f
+systemctl daemon-reload
+systemctl reset-failed
 rm /usr/local/bin/gost
 wget --no-check-certificate -O /etc/systemd/system https://raw.githubusercontent.com/FrankLiangCN/GOST/main/gost.service
 bash <(curl -fsSL https://github.com/go-gost/gost/raw/master/install.sh) --install
