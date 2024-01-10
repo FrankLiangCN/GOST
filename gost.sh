a0f1f2b375ac03107eb490f87b9963e4687f429a
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-01-10 23:24:56 +0800

    Create gost.sh

---
+++ gost.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+systemctl stop gost
+rm /usr/local/bin/gost
+mkdir ./gost
+cd ./gost
+wget https://raw.githubusercontent.com/FrankLiangCN/GOST/main/gost.service
+mv ./gost.service /usr/local/bin/gost
+cd ..
+rm -rf ./gost
+mkdir -p /opt/gost
+bash <(curl -fsSL https://github.com/go-gost/gost/raw/master/install.sh) --install
+systemctl start gost
+systemctl enable gost
