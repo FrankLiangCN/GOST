b2425a60664874ce35c52222fb5027837294c5ae
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-01-11 21:45:12 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -5,9 +5,9 @@ rm /etc/systemd/system/gost.service -f
 systemctl daemon-reload
 systemctl reset-failed
 rm /usr/local/bin/gost
-wget --no-check-certificate -O /etc/systemd/system https://raw.githubusercontent.com/FrankLiangCN/GOST/main/gost.service
+wget --no-check-certificate -P /etc/systemd/system https://raw.githubusercontent.com/FrankLiangCN/GOST/main/gost.service
 bash <(curl -fsSL https://github.com/go-gost/gost/raw/master/install.sh) --install
 mkdir -p /opt/gost
-wget --no-check-certificate -O /opt/gost https://raw.githubusercontent.com/FrankLiangCN/GOST/main/gost.yml
+wget --no-check-certificate -P /opt/gost https://raw.githubusercontent.com/FrankLiangCN/GOST/main/gost.yml
 systemctl start gost
 systemctl enable gost
